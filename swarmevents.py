153b65d310a386b3f1721c9c168fe95819e6960d
Basivireddy <basivireddy@users.noreply.github.com>
2016-06-01 17:05:12 +0530

    Update swarmevents.py

--- swarmevents.py
+++ swarmevents.py
@@ -5,7 +5,7 @@ import subprocess
 import json
 swarmmaster_ip = "127.0.0.1:4000"#os.environ['swarmmaster_ip']
 config = {
-    'host': '54.67.112.100',
+    'host': '52.8.104.253',
     'port': 6379,
     'db': 0,
 }
